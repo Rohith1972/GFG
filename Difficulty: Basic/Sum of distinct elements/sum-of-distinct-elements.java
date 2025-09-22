47e1545f42b8536134c947d3430af749f912f1fc
Rohith1972 <rohith197277@gmail.com>
2025-09-22 23:39:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of distinct elements/sum-of-distinct-elements.java
@@ -0,0 +1,10 @@
+class Solution{
+    int findSum(int arr[]){
+        HashSet<Integer> set=new HashSet<>();
+        int sum=0;
+        for(int x:arr){
+            if(set.add(x))sum+=x;
+        }
+        return sum;
+    }
+}
