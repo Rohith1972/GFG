b45268b8903258059ee76375c6071341c46ddade
Rohith1972 <rohith197277@gmail.com>
2025-09-25 21:09:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Easy string/easy-string.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+class Solution {
+    String transform(String S) {
+        // code here
+        String s = S.toLowerCase();
+        char ch = s.charAt(0);
+        int count=1;
+        StringBuilder res = new StringBuilder();
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==ch)
+                count++;
+            else{
+                res.append(count);
+                res.append(ch);
+                ch = s.charAt(i);
+                count=1;
+            }
+        }
+        res.append(count);
+        res.append(ch);
+        return res.toString();
+    }
+}
