1949141a61f43450efad94a3bbb3a3002335dc5d
Rohith1972 <rohith197277@gmail.com>
2025-10-03 12:31:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Crazy String/crazy-string.java
+++ Difficulty: Basic/Crazy String/crazy-string.java
@@ -4,16 +4,17 @@ class Solution {
     static String getCrazy(String s) {
         char[] ch = s.toCharArray();
         boolean firstIsUpper = Character.isUpperCase(ch[0]);
-
         for (int i = 1; i < ch.length; i++) {
-            if (i % 2 == 0) {
-                // same case as s[0]
-                if (firstIsUpper) ch[i] = Character.toUpperCase(ch[i]);
-                else ch[i] = Character.toLowerCase(ch[i]);
-            } else {
-                // opposite case to s[0]
-                if (firstIsUpper) ch[i] = Character.toLowerCase(ch[i]);
-                else ch[i] = Character.toUpperCase(ch[i]);
+            if(i % 2 == 0) {
+                if(firstIsUpper) 
+                    ch[i] = Character.toUpperCase(ch[i]);
+                else 
+                    ch[i] = Character.toLowerCase(ch[i]);
+            }else{
+                if(firstIsUpper) 
+                    ch[i] = Character.toLowerCase(ch[i]);
+                else 
+                    ch[i] = Character.toUpperCase(ch[i]);
             }
         }
         return String.valueOf(ch);
