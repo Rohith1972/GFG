bb47397cff0a3a1ac793b343feafcf4574beddbc
Rohith1972 <rohith197277@gmail.com>
2025-10-03 10:14:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Crazy String/crazy-string.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+
+class Solution {
+    static String getCrazy(String s) {
+        char[] ch = s.toCharArray();
+        boolean firstIsUpper = Character.isUpperCase(ch[0]);
+
+        for (int i = 1; i < ch.length; i++) {
+            if (i % 2 == 0) {
+                // same case as s[0]
+                if (firstIsUpper) ch[i] = Character.toUpperCase(ch[i]);
+                else ch[i] = Character.toLowerCase(ch[i]);
+            } else {
+                // opposite case to s[0]
+                if (firstIsUpper) ch[i] = Character.toLowerCase(ch[i]);
+                else ch[i] = Character.toUpperCase(ch[i]);
+            }
+        }
+        return String.valueOf(ch);
+    }
+}
