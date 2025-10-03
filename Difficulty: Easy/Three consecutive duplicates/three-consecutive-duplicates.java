7a3067f364b6eb61428a27934deb5323ea8e4ae2
Rohith1972 <rohith197277@gmail.com>
2025-10-03 12:31:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Three consecutive duplicates/three-consecutive-duplicates.java
+++ Difficulty: Easy/Three consecutive duplicates/three-consecutive-duplicates.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 class Solution {
     public String remove3ConsecutiveDuplicates(String S) {
-        // stack storing char and its consecutive frequency
         Stack<Pair> st = new Stack<>();
-        
         for (char c : S.toCharArray()) {
             if (!st.isEmpty() && st.peek().ch == c) {
                 st.peek().count++;
                 if (st.peek().count == 3) {
-                    st.pop(); // remove triplet
+                    st.pop(); 
                 }
             } else {
                 st.push(new Pair(c, 1));
             }
         }
-        
-        // rebuild string
         StringBuilder sb = new StringBuilder();
         for (Pair p : st) {
             for (int i = 0; i < p.count; i++) {
@@ -27,7 +23,6 @@ class Solution {
         return sb.length() == 0 ? "-1" : sb.toString();
     }
 }
-
 class Pair {
     char ch;
     int count;
