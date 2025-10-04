509886aa4aeb56c8e0bd9c67819da60851c2bb4e
Rohith1972 <rohith197277@gmail.com>
2025-10-04 19:46:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Extraction of secret message/extraction-of-secret-message.java
+++ Difficulty: Basic/Extraction of secret message/extraction-of-secret-message.java
@@ -1,8 +1,6 @@
 class Solution {
     String ExtractMessage(String S) {
-        // replace "LIE" with space
         String res = S.replace("LIE", " ");
-        // replace multiple spaces with a single space and trim ends
         res = res.replaceAll("\\s+", " ").trim();
         return res;
     }
