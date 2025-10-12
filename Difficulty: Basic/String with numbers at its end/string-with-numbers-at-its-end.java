4c05358a6b807e4ecd99828b919459bc8dad1221
Rohith1972 <rohith197277@gmail.com>
2025-10-12 11:18:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/String with numbers at its end/string-with-numbers-at-its-end.java
+++ Difficulty: Basic/String with numbers at its end/string-with-numbers-at-its-end.java
@@ -1,21 +1,22 @@
 // User function Template for Java
 
 class Solution {
-    int isSame(String s){
-        int n = s.length();
-        int i = n - 1;
-        while (i >= 0 && Character.isDigit(s.charAt(i))) {
-            i--;
+    int isSame(String s) {
+        // code here
+        int i=0;
+        int length=0;
+        while(Character.isLetter(s.charAt(i))){
+            length++;
+            i++;
         }
-        String strPart = s.substring(0, i + 1);
-        String numPart = s.substring(i + 1);
-        if (numPart.length() == 0)
-            return 0; 
-        try {
-            long num = Long.parseLong(numPart);  
-            return strPart.length() == num ? 1 : 0;
-        } catch (NumberFormatException e) {
+        long num = 0;
+        try{
+            num = Long.parseLong(s.substring(i,s.length()));
+        }catch(NumberFormatException e){
             return 0;
         }
+        if(length == num)
+            return 1;
+        return 0;
     }
 }
