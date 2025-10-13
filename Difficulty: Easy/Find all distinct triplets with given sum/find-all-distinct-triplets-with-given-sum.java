015da53332bd6c90b80e66e583bf16665b614da7
Rohith1972 <rohith197277@gmail.com>
2025-10-13 21:51:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find all distinct triplets with given sum/find-all-distinct-triplets-with-given-sum.java
@@ -0,0 +1,34 @@
+// User function Template for Java
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums, int target) {
+        // Your code here
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
+            int l=i+1,h=nums.length-1;
+            while(l<h){
+                int sum = nums[i]+nums[l]+nums[h];
+                if(sum == target){
+                    List<Integer> t = new ArrayList<>();
+                    t.add(nums[i]);
+                    t.add(nums[l]);
+                    t.add(nums[h]);
+                    res.add(t);
+                    l++;
+                    h--;
+                    while (l < h && nums[l] == nums[l - 1]) 
+                        l++;
+                    while (l < h && nums[h] == nums[h + 1]) 
+                        h--;
+                }else if(sum < target){
+                    l++;
+                }else{
+                    h--;
+                }
+            }
+        }
+        return res;
+    }
+}
