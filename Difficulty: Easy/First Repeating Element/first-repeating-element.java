cbec2139c6febadadc6d6d8fc9a102cfbe3bbcc8
Rohith1972 <rohith197277@gmail.com>
2025-10-29 17:09:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int firstRepeated(int[] arr) {
+        // code here
+        HashMap<Integer, Integer> firstIndexMap = new HashMap<>();
+        int minIndex = Integer.MAX_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            if (firstIndexMap.containsKey(arr[i])) {
+                minIndex = Math.min(minIndex, firstIndexMap.get(arr[i]));
+            }else{
+                firstIndexMap.put(arr[i], i); 
+            }
+        }
+        return (minIndex == Integer.MAX_VALUE) ? -1 : (minIndex + 1);
+    }
+}
