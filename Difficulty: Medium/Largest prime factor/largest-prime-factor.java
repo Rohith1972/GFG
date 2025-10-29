058d9cf694d168eb6bea76a3c06b01a48f60ba2a
Rohith1972 <rohith197277@gmail.com>
2025-10-29 16:09:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest prime factor/largest-prime-factor.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+class Solution {
+    static int largestPrimeFactor(int n) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        if(n%2==0){
+            al.add(2);
+            while(n%2==0){
+            n/=2;
+        }
+        }
+        for(int i=3;i*i<=n;i+=2){
+            if(n%i==0){
+                al.add(i);
+                while(n%i==0){
+                    n/=i;
+                }
+            }
+        }
+        if(n>1)
+            al.add(n);
+        int res = al.size();
+        return al.get(res-1);
+    }
+}
