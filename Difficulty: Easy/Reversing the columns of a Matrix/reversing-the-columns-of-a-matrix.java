b1eec1b554cbf7192d02ab7ccea59c40a217b77b
Rohith1972 <rohith197277@gmail.com>
2025-10-30 09:41:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reversing the columns of a Matrix/reversing-the-columns-of-a-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    static void reverseCol(int matrix[][]){
+        int n = matrix.length;
+        if (n == 0) 
+        return;
+        int m = matrix[0].length;
+        if (m <= 1) return;
+        for(int i=0;i<n;i++){
+            int left = 0;
+            int right = m - 1;
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
