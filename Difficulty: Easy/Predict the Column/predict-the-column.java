e72d9d3762b7340bd40c887da95dd488196c4fac
Rohith1972 <rohith197277@gmail.com>
2025-10-30 09:32:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Predict the Column/predict-the-column.java
+++ Difficulty: Easy/Predict the Column/predict-the-column.java
@@ -2,19 +2,17 @@ class Solution {
     int columnWithMaxZeros(int M[][], int N) {
         int maxCount = 0;
         int colIndex = -1;
-
-        for (int j = 0; j < N; j++) {
+        for(int j=0;j<N;j++){
             int count = 0;
-            for (int i = 0; i < N; i++) {
+            for(int i=0;i<N;i++){
                 if (M[i][j] == 0)
                     count++;
             }
-            if (count > maxCount) {
+            if(count > maxCount){
                 maxCount = count;
                 colIndex = j;
             }
         }
-
         return (maxCount == 0) ? -1 : colIndex;
     }
 }
