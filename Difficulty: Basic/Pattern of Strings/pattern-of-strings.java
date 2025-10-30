b5fa7793573a8e731c717e91a9414aaddad61b18
Rohith1972 <rohith197277@gmail.com>
2025-10-30 09:52:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern of Strings/pattern-of-strings.java
@@ -0,0 +1,9 @@
+class Solution {
+    ArrayList<String> pattern(String s){
+        ArrayList<String> result=new ArrayList<>();
+        for (int i=s.length();i >= 1;i--){
+            result.add(s.substring(0, i));
+        }
+        return result;
+    }
+}
