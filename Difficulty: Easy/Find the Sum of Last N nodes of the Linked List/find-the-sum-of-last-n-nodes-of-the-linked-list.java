3daa28fb4e03eb2bff906ec07309ee19f2c30a3a
Rohith1972 <rohith197277@gmail.com>
2026-02-22 22:49:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.java
@@ -0,0 +1,36 @@
+/*
+Node class is as follows:
+
+class Node {
+    int data;
+    Node next;
+
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+
+    // Return the sum of last k nodes
+    public int sumOfLastN_Nodes(Node head, int n) {
+        // write code here
+        Node fast = head;
+        Node slow = head;
+        for(int i=0;i<n;i++){
+            fast = fast.next;
+        }
+        while(fast!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        int sum = 0;
+        while(slow!=null){
+            sum += slow.data;
+            slow = slow.next;
+        }
+        return sum;
+    }
+}
